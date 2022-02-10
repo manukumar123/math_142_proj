5a934990a414596b2fab8fda7c15bb13674d0df5
Manu Kumar <mangalampalli.manu@gmail.com>
2022-02-10 12:49:22 -0800

    Added reinfection rate

--- math_142_sims.m
+++ math_142_sims.m
@@ -1,4 +1,4 @@
-syms m n g b
+syms m n g b s
 
 % Jacobian of SIRRe model, b = beta * s
 J(g, b, n, m) = [0, -b+g, 0, 0, 0;
@@ -6,4 +6,13 @@ J(g, b, n, m) = [0, -b+g, 0, 0, 0;
                  0, g, -m, 0, 0;
                  0, n, 0, 0, 0;
                  0, 0, m, 0, 0;];
-[V, D] = eig(J)
+[V, D] = eig(J);
+
+
+% stability analysis of bs_0 < 4m
+m_func(b, s) = (b*s) / 4;
+b_prac = 0:0.01:1;
+s_prac = b_prac;
+m_prac = m_func(b_prac, s_prac);
+plot3(b_prac, s_prac, m_prac);
+
