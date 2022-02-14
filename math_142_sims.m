53c54a52afb9092ad9b7b732d8b62ff7966113bb
Manu Kumar <mangalampalli.manu@gmail.com>
2022-02-14 13:53:33 -0800

    Compiled pdf without proportionality analysis.

--- math_142_sims.m
+++ math_142_sims.m
@@ -6,13 +6,4 @@ J(g, b, n, m) = [0, -b+g, 0, 0, 0;
                  0, g, -m, 0, 0;
                  0, n, 0, 0, 0;
                  0, 0, m, 0, 0;];
-[V, D] = eig(J);
-
-
-% stability analysis of bs_0 < 4m
-m_func(b, s) = (b*s) / 4;
-b_prac = 0:0.01:1;
-s_prac = b_prac;
-m_prac = m_func(b_prac, s_prac);
-plot3(b_prac, s_prac, m_prac);
-
+[V, D] = eig(J)
